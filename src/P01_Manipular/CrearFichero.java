50973d440c1852e761bc9a581845e0a34d0092c6
ifc <ifc@DESKTOP-E4EJII0>
2018-09-26 19:08:29 +0200

    Ejercicios iniciales con ficheros

---
+++ src/P01_Manipular/CrearFichero.java
@@ -0,0 +1,31 @@
+package P01_Manipular;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CrearFichero {
+
+	public static void main(String[] args) 
+	{
+		
+		try
+		{
+			FileWriter fichero = new FileWriter("src\\P01_Manipular\\fichero.txt");
+			
+			String dir = "src\\P01_Manipular\\carpeta";
+			
+		}
+		catch(IOException e)
+		{
+			
+		}
+		
+
+		
+	
+		
+	
+		
+	}
+
+}
---
+++ src/P02_Metodos/Metodos.java
@@ -0,0 +1,27 @@
+package P02_Metodos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+			FileWriter fichero = new FileWriter("src\\P02_Metodos\\fichero.txt");
+			
+			fichero.write("¿Qué es el Cipotegato?");
+			
+			fichero.close();
+		}
+		catch(IOException e)
+		{
+			
+		}
+	
+		
+
+	}
+
+}
