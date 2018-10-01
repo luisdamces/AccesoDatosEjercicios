fc0e2b8ca4a8447b56df5a43cae9848b11840622
ifc <ifc@DESKTOP-E4EJII0>
2018-10-01 17:21:26 +0200

    Ejercicio de escribir 10 provincias en un fichero

--- src/P03_lectura_escritura/Lectura.java
+++ src/P03_lectura_escritura/Lectura.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class LecturaEscritura {
+public class Lectura {
 
 	public static void main(String[] args) throws IOException{
 
