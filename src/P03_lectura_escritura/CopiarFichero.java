3cdb55eeda1d9578518729f1008963692df87ae5
ifc <ifc@DESKTOP-E4EJII0>
2018-10-03 19:16:13 +0200

    Ejercicio en el que se copia el texto de un fichero a otro

---
+++ src/P03_lectura_escritura/CopiarFichero.java
@@ -0,0 +1,49 @@
+package P03_lectura_escritura;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopiarFichero {
+
+	public static void main(String[] args) throws IOException{
+
+		File f1 = new File("src\\P03_lectura_escritura\\f1.txt");
+			f1.createNewFile();
+		File f2 = new File("src\\P03_lectura_escritura\\f2.txt");
+			f2.createNewFile();
+			
+		int i = 0;
+		
+		String texto = "";
+		
+		char caracteres[];
+		
+		//--------LEER EL PRIMERO FICHERO-----------------------------
+		
+		FileReader fr = new FileReader(f1);
+		
+		while((i=fr.read())!=-1)//while para leer el fichero
+		{
+				texto = texto + (char)i;
+		}
+		
+		fr.close();
+		
+		//---------ESCRIBIR EL TEXTO EN EL SEGUNDO--------------------
+		
+		FileWriter fw = new FileWriter(f2);
+		
+		caracteres = texto.toCharArray();
+		
+		for(int x = 0; x<caracteres.length; x++)
+		{
+			fw.write(caracteres[x]);
+		}
+
+		fw.close();
+		
+	}
+
+}
