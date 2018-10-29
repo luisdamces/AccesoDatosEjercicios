0e6219c5d507eced0d121b16247da31de7321dcf
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-10-29 17:39:40 +0100

    Clase CrearCancionXML para crear fichero XML (sin terminar)

---
+++ src/P05_FicherosXML/CrearCancionXML.java
@@ -0,0 +1,115 @@
+package P05_FicherosXML;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+public class CrearCancionXML {
+
+	public static void main(String[] args) throws IOException {
+
+	//creamos variable fichero y flujo de lectura de fichero de objetos
+	
+	File f = new File("src\\P05_FicherosXML\\ficheros\\canciones.dat");
+	
+	FileInputStream fileIn = new FileInputStream(f);
+	ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+	
+	
+	//creamos variables 
+
+	Cancion cancion = new Cancion();
+	
+	
+	//instancia para construir el parser
+	
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	
+	
+	try
+	{
+		
+		//creamos varias objetos para crear finalmente el documento xml
+		
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		DOMImplementation implementation = builder.getDOMImplementation();
+		
+		Document document = implementation.createDocument(null,"Canciones",null);
+			document.setXmlVersion("1.0");
+			
+			
+		for(int i = 0; i < 5; i++)
+		{
+			
+			cancion = (Cancion) objectIn.readObject();
+			
+			//creamos nodo raiz cancion
+			Element raiz = document.createElement("Cancion");
+			
+			//añadimos nodo principal 
+			document.getDocumentElement().appendChild(raiz);
+			
+			//
+			
+			//creamos elemento id
+			crearElemento("Id",Integer.toString(cancion.getId()),raiz,document);
+			
+			//creamos elemento año
+			crearElemento("Año",Integer.toString(cancion.getAno()),raiz,document);
+			
+			//creamos elemento titulo
+			crearElemento("Titulo", cancion.getTitulo(), raiz, document);
+			
+			//creamos elemento artista
+			crearElemento("Artista", cancion.getArtista(), raiz, document);
+			
+			//creamos elemento duracion
+			crearElemento("Duracion", cancion.getDuracion(), raiz, document);
+			
+			//creamos elemento es cancion española?
+			crearElemento("Española", 
+						  Boolean.toString(cancion.isCancion_espanola()), 
+					      raiz, document);
+			
+		}
+		
+		
+	}
+	catch (Exception e)
+	{
+		System.out.println("Ha habido un error");
+	}
+
+}
+	
+	/**
+	 * Metodo que crea un elemento para escribirlo en documento xml
+	 * @param datoCancion Elemento de la canción
+	 * @param valor Valor que se va a dar al elemento
+	 * @param raiz Elemento raiz
+	 * @param document Documento donde se van a añadir estos elementos
+	 */
+	static void crearElemento(String datoCancion, String valor, Element raiz,
+							  Document document)
+	{
+		
+		Element elemento = document.createElement(datoCancion);
+		Text texto = document.createTextNode(valor); //damos valor
+		
+		raiz.appendChild(elemento); //pegamos el elemento hijo a la raiz
+		elemento.appendChild(texto); //pegamos el valor al elemento hijo
+		
+	}
+
+}
