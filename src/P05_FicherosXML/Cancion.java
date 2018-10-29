493c7ed6cfa3ef369551453a1ec0795bd248dcb4
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-10-29 16:55:20 +0100

    Clase "Cancion" y clase para crear el fichero de objetos con 5 canciones

---
+++ src/P05_FicherosXML/Cancion.java
@@ -0,0 +1,115 @@
+package P05_FicherosXML;
+
+import java.io.Serializable;
+
+public class Cancion implements Serializable{
+	
+	private int id; // 4B
+	private int ano; // 4B
+	private String titulo; //20 char -> 2B * 20 = 40 B
+	private String artista; //20 char -> 2B * 20 = 40 B
+	private String duracion; //20 char -> 2B * 20 = 40 B
+	private boolean cancion_espanola; // 1B
+	
+	// TAMAÑO TOTAL CANCION = 129 Bytes;
+	
+	Cancion(int vid, int vano, String vtitulo, String vartista, String vduracion, boolean vcancion_espanola)
+	{
+		id = vid;
+		ano = vano;
+		titulo = vtitulo;
+		artista = vartista;
+		duracion = vduracion;
+		cancion_espanola = vcancion_espanola;
+	}
+	
+	Cancion()
+	{
+		id = 0;
+		ano = 0;
+		titulo = "";
+		artista = "";
+		duracion = "";
+		cancion_espanola = false;
+	}
+	
+	//GETTERS Y SETTERS -
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public String getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+
+	public boolean isCancion_espanola() {
+		return cancion_espanola;
+	}
+
+	public void setCancion_espanola(boolean cancion_espanola) {
+		this.cancion_espanola = cancion_espanola;
+	}
+	
+	//
+	
+	public String mostrarDatosCancion()
+	{
+		return "" + getId() + " - " + getAno() + " - " + acortarString(getTitulo()) + " - " + 
+				acortarString(getArtista()) + " - " + acortarString(getDuracion()) + " - " + isCancion_espanola() + "";
+	}
+	
+	public String acortarString(String cadena)
+	{
+		char[] car;
+		String str = "";
+		car = cadena.toCharArray();
+		
+		for(int i = cadena.length()-1; i>=0; i--)
+		{
+			if(Character.isLetterOrDigit((car[i])))
+			{
+				for(int x = 0; x < i+1; x++)
+				{
+					str = str + car[x];
+				}break;
+			}
+		}
+		
+		return str;
+		
+	}
+
+}
---
+++ src/P05_FicherosXML/Crear_Fichero_canciones.java
@@ -0,0 +1,35 @@
+package P05_FicherosXML;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Crear_Fichero_canciones {
+
+	public static void main(String[] args) throws IOException {
+
+		File f = new File("src\\P05_FicherosXML\\ficheros\\canciones.dat");
+		f.createNewFile();
+		
+		FileOutputStream fileOut = new FileOutputStream(f);
+		ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+
+		Cancion canciones[] = new Cancion[5];
+		
+		canciones[0] = new Cancion(1, 2002, "Plug n baby", "Muse", "2:40", false);
+		canciones[1] = new Cancion(2, 2013, "R u mine", "Arctic Monkeys", "3:10", false);
+		canciones[2] = new Cancion(3, 2014, "Loose Sutures", "Fuzz", "4:30", false);
+		canciones[3] = new Cancion(4, 2018, "Total Football", "Parquet Courts", "3:00", false);
+		canciones[4] = new Cancion(5, 1973, "Entre dos aguas", "Paco de lucia", "6:00", true);
+		
+		for(int i = 0; i < canciones.length; i++)
+		{
+			objectOut.writeObject(canciones[i]);
+		}
+		
+		objectOut.close();
+
+	}
+
+}
