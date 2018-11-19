34174df10c22aa63e0dab260d83ba066cba8e0d4
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-11-19 17:38:47 +0100

    Update. La clase ahora inserta, borra, y busca registros.

--- src/UD02_P02_ConectoresSQLite/ConectorSQLite.java
+++ src/UD02_P02_ConectoresSQLite/ConectorSQLite.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 public class ConectorSQLite {
 	
 	private static Statement statement;
+	private static ResultSet rs;
 	
 	  public static void main(String[] args)
       {
@@ -31,14 +32,18 @@ public class ConectorSQLite {
           	 + "email string, telefono string, localidad string)");
           
           insertar("oscar", "tutor", "a@a.es", "898787676", "tudela");
+          insertar("oscars", "tutors", "a@a.es", "898787676", "tudelas");
+          insertar("pepe", "pepito", "a@a.es", "898787676", "pepe");
           
-          ResultSet rs = statement.executeQuery("select * from persona");
-          while(rs.next())
-          {
-            // read the result set
-            System.out.println("name = " + rs.getString("name"));
-            System.out.println("id = " + rs.getInt("id"));
-          }
+          consultar();
+          
+          buscar(2);
+          
+          borrar(2);
+          
+          consultar();
+          
+         
         }
         catch(SQLException e)
         {
@@ -61,6 +66,7 @@ public class ConectorSQLite {
         }
       }
 	  
+	  
 	  /**
 	   * Metodo que inserta un registro en la tabla personas
 	   * @param nombre
@@ -68,13 +74,103 @@ public class ConectorSQLite {
 	   * @param email
 	   * @param telefono
 	   * @param localidad
-	 * @throws SQLException 
+	   * @throws SQLException 
 	   */
 	  public static void insertar(String nombre, String apellido, String email, 
 		                   String telefono, String localidad) throws SQLException
 	  {
-		  statement.executeUpdate("insert into persona values(2, '"+nombre+"', "
+		  int id = 1;
+		  
+		  //comprobar el ultimo id de la base de datos
+		  rs = statement.executeQuery("select id from persona ORDER BY id ASC");
+		  
+		  //incrementa en 1 el id para que sea autoincrementado
+          while(rs.next())
+          {
+        	  id = rs.getInt("id");
+        	  id++;
+          }
+		  
+		  statement.executeUpdate("insert into persona values("+id+", '"+nombre+"', "
 		  		+ "'"+apellido+"','"+email+"', '"+telefono+"','"+localidad+"')");
 	  }
+	  
+	  
+	  /**
+	   * Metodo que saca por pantalla todos los registros de la base de datos
+	   * @throws SQLException Excepcion
+	   */
+	  public static void consultar() throws SQLException
+	  {
+		  //consulta a base de datos
+		  rs = statement.executeQuery("select * from persona");
+		  
+		  System.out.println("Registros de la base de datos: ");
+		  
+          while(rs.next())
+          {
+            // read the result set
+        	System.out.print("id = " + rs.getInt("id") + " | ");
+            System.out.print("nombre = " + rs.getString("nombre") + " | ");
+            System.out.print("apellido = " + rs.getString("apellido") + " | ");
+            System.out.print("email = " + rs.getString("email") + " | ");
+            System.out.print("telefono = " + rs.getString("telefono") + " | ");
+            System.out.println("localidad = " + rs.getString("localidad"));       
+          }
+	  }
 
+	  
+	  /**
+	   * Metodo que busca un registro a partir de un id
+	   * @param id Id para buscar un registro
+	   * @throws SQLException Excepcion
+	   */
+	  public static void buscar(int id) throws SQLException
+	  {
+		  //consulta a base de datos
+		  rs = statement.executeQuery("select * from persona where id = '"+id+"'");
+		  
+		  System.out.println("Registro con ID " + id +":");
+		  
+          while(rs.next())
+          {
+            // read the result set
+        	System.out.print("id = " + rs.getInt("id") + " | ");
+            System.out.print("nombre = " + rs.getString("nombre") + " | ");
+            System.out.print("apellido = " + rs.getString("apellido") + " | ");
+            System.out.print("email = " + rs.getString("email") + " | ");
+            System.out.print("telefono = " + rs.getString("telefono") + " | ");
+            System.out.println("localidad = " + rs.getString("localidad"));       
+          }
+	  }
+
+	  
+	  /**
+	   * Metodo que elimina un registro de la tabla a partir de un ID
+	   * @param id
+	   * @throws SQLException Excepcion
+	   */
+	  public static void borrar(int id)
+	  {
+		  try
+		  {
+			  //borrar un registro a partir del id
+			  rs = statement.executeQuery("delete from persona where id = '"+id+"'");
+			  
+			  rs = statement.executeQuery("select * from persona where id = '"+id+"'");
+			  
+			  if(rs.next())
+			  {
+				 
+			  }
+			  else
+			  {
+				  System.out.println("El registro se ha borrado"); 
+			  }
+		  }
+		  catch(SQLException e)
+		  {  
+		  }
+
+	  }
 }
