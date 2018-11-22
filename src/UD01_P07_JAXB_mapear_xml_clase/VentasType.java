fc063e2d88991a66f8ee8f91c86272e07f091ea9
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-11-22 22:10:01 +0100

    Actualizacions de las clases de ventas

--- src/UD01_P07_JAXB_mapear_xml_clase/VentasType.java
+++ src/UD01_P07_JAXB_mapear_xml_clase/VentasType.java
@@ -1,8 +1,8 @@
 //
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantaci√≥n de la referencia de enlace (JAXB) XML v2.2.11 
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantaciÛn de la referencia de enlace (JAXB) XML v2.2.11 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perder√°n si se vuelve a compilar el esquema de origen. 
-// Generado el: 2018.11.12 a las 04:41:03 PM CET 
+// Todas las modificaciones realizadas en este archivo se perder·n si se vuelve a compilar el esquema de origen. 
+// Generado el: 2018.11.22 a las 09:58:08 PM CET 
 //
 
 
