a4d20f710557623e9d3d505414017ec1b6c24082
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-01-09 19:39:26 +0100

    Hibernate configurado y clases con pruebas de consultas sobre BBDD

--- src/UD03_P01_ORM/E01_PruebaHibernate.java
+++ src/UD03_P01_ORM/E01_PruebaHibernate.java
@@ -3,7 +3,11 @@ package UD03_P01_ORM;
 //importamos el modelo-entidad de la tabla departamentos
 //que hemos generado con Hibernate
 import modelos.entidades.*;
+
+import java.util.List;
+
 import org.hibernate.*;
+
 
 //clase necesaria para el funcionamiento de hibernate
 import modelo.HibernateUtils;
@@ -15,14 +19,27 @@ public class E01_PruebaHibernate {
 		//generamos el objeto SessionFactory a partir de la clase HibernateUtilss
 		SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
 		//abrimos la sesi√≥n
-		Session sesion = sessionFactory.openSession();
+		Session session = sessionFactory.openSession();
 		//genera conexion entre aplicacion y base de datos
-		Transaction tr = sesion.beginTransaction();
+		Transaction tr = session.beginTransaction();
 		
-		Jugadores jugador = new Jugadores();
-		jugador.setNombre("Oscar Tutor");
-		sesion.save(jugador);
+		//insertar jugador
+		Jugadores jugador = new Jugadores(616, null, "Alex", "Novallas", "7-2", 300, "C");
+		session.save(jugador);
 		tr.commit();
+		
+		//obtener datos de un jugador
+		Jugadores jugador2 = (Jugadores) session.get(Jugadores.class, 614);
+		System.out.println("CÛdigo: " + jugador2.getCodigo() + " - Nombre: " + jugador2.getNombre());
+		
+		//obtener datos de un jugador con query
+		Query query = session.createQuery("SELECT j FROM Jugadores j");
+		List<Jugadores> jugadores = query.list();
+		
+		for (Jugadores jugador : jugadores) {
+		System.out.println(jugador.toString());
+		}
+		
 		sessionFactory.close();
 	}
 }
