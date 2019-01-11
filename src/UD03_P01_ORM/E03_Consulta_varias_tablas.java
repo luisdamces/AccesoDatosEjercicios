9d6b784ef2681d6b1d4fe5d1f6009a4a4b065104
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-01-11 20:12:46 +0100

    Terminada consulta que obtiene estadisticas sobre el jugador

--- src/UD03_P01_ORM/E03_Consulta_varias_tablas.java
+++ src/UD03_P01_ORM/E03_Consulta_varias_tablas.java
@@ -33,14 +33,29 @@ public class E03_Consulta_varias_tablas {
 		clave = teclado.nextLine();
 		
 		//obtener datos de un jugador
-		Query query1 = session.createQuery("SELECT Jugadores.nombre, Equipos.ciudad FROM Jugadores, Equipos "
-				+ "where Jugadores.nombre_equipo = Equipos.nombre and Jugadores.codigo = 1");
+		/*Query query1 = session.createQuery("SELECT Jugadores.nombre, Equipos.ciudad FROM Jugadores, Equipos "
+				+ "where Jugadores.nombre_equipo = Equipos.nombre and Jugadores.codigo = 1");*/
+		Query query1 = session.createQuery("FROM Jugadores where codigo = '"+ clave + "'");
 		List<Jugadores> jugadores = query1.list();
+		
+		System.out.println("---DATOS DEL JUGADOR---");
 		
 		for (Jugadores jugador : jugadores) {	
 		System.out.println("Nombre: " + jugador.getNombre() + " - Altura: " + jugador.getAltura() + 
 				" - Posición: " + jugador.getPosicion() + " - Peso: " + jugador.getPeso() + 
 				" - Equipo: " + jugador.getEquipos() + " - Ciudad del equipo: " );
+		}
+		
+		//obtener estadisticas del jugador
+		Query query2 = session.createQuery("FROM Estadisticas where jugador = '" + clave + "'");
+		List<Estadisticas> estadisticas = query2.list();
+		
+		System.out.println("---ESTADISTICAS DEL JUGADOR---");
+		
+		for (Estadisticas estadistica : estadisticas) {	
+		System.out.println("Temporada: " + estadistica.getTemporada() + " - Puntos por partido: " + estadistica.getPuntos_por_partido() +
+				" - Asistencias por partido: " + estadistica.getAsistencias_por_partido() + " - Tapones por partido: " + estadistica.getTapones_por_partido() + 
+				" - Rebotes por partido: " + estadistica.getRebotes_por_partido());
 		}
 		
 		sessionFactory.close();
