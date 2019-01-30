a8843c894235f9fab4b23bc9c1ce2b8316720656
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-01-30 19:03:11 +0100

    Ejemplo de paso de datos de b.d. mysql a neodatis en java

---
+++ src/UD04_P02_BDOO_MySQL_a_Neodatis/E01_C1Asignaturas.java
@@ -0,0 +1,41 @@
+package UD04_P02_BDOO_MySQL_a_Neodatis;
+
+import java.util.Set;
+
+public class E01_C1Asignaturas {
+
+	private String codAsig;
+	private String nombreAsi;
+	private Set<E01_C1Profesores> setprofesores;
+	
+	public E01_C1Asignaturas(String codAsig, String nombreAsi, Set<E01_C1Profesores> setprofesores) {
+		super();
+		this.codAsig = codAsig;
+		this.nombreAsi = nombreAsi;
+		this.setprofesores = setprofesores;
+	}
+	
+	public E01_C1Asignaturas(){}
+	
+	//GETTERS y SETTERS
+	
+	public String getCodAsig() {
+		return codAsig;
+	}
+	public void setCodAsig(String codAsig) {
+		this.codAsig = codAsig;
+	}
+	public String getNombreAsi() {
+		return nombreAsi;
+	}
+	public void setNombreAsi(String nombreAsi) {
+		this.nombreAsi = nombreAsi;
+	}
+	public Set<E01_C1Profesores> getSetprofesores() {
+		return setprofesores;
+	}
+	public void setSetprofesores(Set<E01_C1Profesores> setprofesores) {
+		this.setprofesores = setprofesores;
+	}
+
+}
---
+++ src/UD04_P02_BDOO_MySQL_a_Neodatis/E01_C1Centros.java
@@ -0,0 +1,88 @@
+package UD04_P02_BDOO_MySQL_a_Neodatis;
+
+import java.util.Set;
+
+public class E01_C1Centros {
+
+	private Integer codCentro;
+	private String nomCentro;
+	private E01_C1Profesores director;
+	private String direccion;
+	private String localidad;
+	private String provincia;
+	private Set<E01_C1Profesores> setprofesores;
+	
+	public E01_C1Centros(Integer codCentro, String nomCentro, E01_C1Profesores director, String direccion, String localidad,
+			String provincia, Set<E01_C1Profesores> setprofesores) {
+		super();
+		this.codCentro = codCentro;
+		this.nomCentro = nomCentro;
+		this.director = director;
+		this.direccion = direccion;
+		this.localidad = localidad;
+		this.provincia = provincia;
+		this.setprofesores = setprofesores;
+	}
+	
+	public E01_C1Centros(){}
+
+	//GETTERS y SETTERS
+	
+	public Integer getCodCentro() {
+		return codCentro;
+	}
+
+	public void setCodCentro(Integer codCentro) {
+		this.codCentro = codCentro;
+	}
+
+	public String getNomCentro() {
+		return nomCentro;
+	}
+
+	public void setNomCentro(String nomCentro) {
+		this.nomCentro = nomCentro;
+	}
+
+	public E01_C1Profesores getDirector() {
+		return director;
+	}
+
+	public void setDirector(E01_C1Profesores director) {
+		this.director = director;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public Set<E01_C1Profesores> getSetprofesores() {
+		return setprofesores;
+	}
+
+	public void setSetprofesores(Set<E01_C1Profesores> setprofesores) {
+		this.setprofesores = setprofesores;
+	}
+	
+
+}
